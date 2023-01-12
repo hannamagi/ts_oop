5fc97141394a341c76c0e522e16113cca9002eca
Hanna <hanna.magi.001@voco.ee>
2023-01-12 08:57:34 +0200

    Ülesanne TS 1

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,4 +1,4 @@
-class Resistorrun {
+class Resistor {
     r: number = 0;
     constructor(r: number) {
         this.r = r;
@@ -8,5 +8,16 @@ class Resistorrun {
     }
 }
 
-let r1: Resistorrun = new Resistorrun(220);
+let r1: Resistor = new Resistor(220);
 console.log(r1.getCurrent(5));
+let r2: Resistor = new Resistor(4700);
+console.log(r2.getCurrent(5));
+let r3: Resistor = new Resistor(110);
+console.log(r3.getCurrent(5));
+
+let resistors: Array<Resistor> = [r1, r2, r3];
+let totalAmperage: number = 0;
+resistors.forEach((resistor) => {
+    totalAmperage += resistor.getCurrent(5)
+});
+console.log(totalAmperage)
